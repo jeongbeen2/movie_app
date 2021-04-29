cbe9194152a1be9bd2b6b4b82e7c8242584a8979
jeongbeen2 <aceman9508@gmail.com>
2021-04-29 11:59:19 +0900

    react app structure understand

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    this.setState({ movies, isLoading: false }); // 여기의 movies는 불러온 데이터를 의미.
+  };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 2000);
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? 'Loading' : 'We are ready'}</div>;
+    const { isLoading, movies } = this.state; // state에서 isLoading과 movie를 가져온다.
+    // render 내에서 isLoading, movies 등 state에서 정의해준 것을 가져와야 쓸 수 있다. 꼭 기억하기
+    return (
+      <section className="container">
+        <div>
+          {isLoading ? (
+            <div className="loader">
+              <span className="loader__text">Loading...</span>
+            </div>
+          ) : (
+            <div className="movies">
+              {movies.map((movie) => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    );
   }
 }
 
