2529a4a5d32346b36228c783d2223661de22abbe
jeongbeen2 <aceman9508@gmail.com>
2021-04-29 21:23:39 +0900

    movie app done

--- src/App.js
+++ src/App.js
@@ -1,60 +1,20 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Detail from './routes/Detail';
+import Navigation from './components/Navigation';
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
-    );
-    this.setState({ movies, isLoading: false }); // 여기의 movies는 불러온 데이터를 의미.
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state; // state에서 isLoading과 movie를 가져온다.
-    // render 내에서 isLoading, movies 등 state에서 정의해준 것을 가져와야 쓸 수 있다. 꼭 기억하기
-    return (
-      <section className="container">
-        <div>
-          {isLoading ? (
-            <div className="loader">
-              <span className="loader__text">Loading...</span>
-            </div>
-          ) : (
-            <div className="movies">
-              {movies.map(
-                (movie) => (
-                  console.log(movie.summary),
-                  (
-                    <Movie
-                      key={movie.id}
-                      id={movie.id}
-                      year={movie.year}
-                      title={movie.title}
-                      summary={movie.summary}
-                      poster={movie.medium_cover_image}
-                      genres={movie.genres}
-                    />
-                  )
-                )
-              )}
-            </div>
-          )}
-        </div>
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
